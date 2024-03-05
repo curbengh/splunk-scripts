99cdb6c4a59e321ac9b517423e38624d276ae84b
Ming Di Leom <2809763-curben@users.noreply.gitlab.com>
2024-03-05 07:28:19 +0000

    fix(nmap): save to monitored folder only after sed

--- nmap/bin/nmap.sh
+++ nmap/bin/nmap.sh
@@ -3,14 +3,19 @@
 ports="21 23"
 subnets="192.168.1.0/24 192.168.2.0/24 192.168.3.0/24"
 
-mkdir -p "$SPLUNK_HOME/var/log/nmap"
-cd "$SPLUNK_HOME/var/log/nmap"
+mkdir -p "$SPLUNK_HOME/var/log/nmap/"
 
 for port in $ports; do
-  filename="nmap_port${port}_$(date +%Y%m%d-%H%M%S).xml"
-  nmap -p"$port" -sV -oX "$filename" -R --system-dns $subnets > /dev/null
+  xml_location="/tmp/nmap_port${port}_$(date +%Y%m%d-%H%M%S).xml"
+
+  nmap -p"$port" -sV -oX "$xml_location" -R --system-dns $subnets > /dev/null
 
   # "-z" requires GNU sed
   # remove prefix, suffix, host status (so that it port state is parsed instead)
-  sed -z -u -e 's|.*</hosthint>\n<host |<host |g' -e 's|<runstats>.*||g' -e 's|<status state="[[:alpha:]]+" reason="[[:alpha:]-]+" reason_ttl="[[:digit:]]+"/>||g' -i "$filename"
+  # hosthint element may not exist
+  sed -r -z -u -e 's@.*(</hosthint>|<debugging level="[[:digit:]]+"/>)\n<host @<host @g' -e 's|<runstats>.*||g' -e 's|<status state="[[:alpha:]]+" reason="[[:alpha:]-]+" reason_ttl="[[:digit:]]+"/>||g' -i "$xml_location"
+
+  # save to monitored folder only after sed,
+  # otherwise splunk will ingest partial output while nmap is running
+  cp "$xml_location" "$SPLUNK_HOME/var/log/nmap/"
 done

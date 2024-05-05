2c856f90489347db5ab46883dfb349d75cba7c0d
Ming Di Leom <2809763-curben@users.noreply.gitlab.com>
2024-05-05 03:49:22 +0000

    feat(uf): clean install
    to unify installation directory

--- universal-forwarder/install_universal_forwarder.ps1
+++ universal-forwarder/install_universal_forwarder.ps1
@@ -1,15 +1,17 @@
 Function Rmf {
     foreach ($arg in $args) {
-        if (test-path -path $arg) { remove-item -path $arg }
+        if (test-path -path $arg) { remove-item -Recurse -Force -path $arg }
     }
 }
 
 set-location -path "$PSScriptRoot"
+$uf_dir = "$env:programfiles\SplunkUniversalForwarder"
 $argument_list = @(
     "/i",
     "PATH_TO_MSI",
     "USE_LOCAL_SYSTEM=1",
     "AGREETOLICENSE=Yes",
+    "INSTALLDIR=""$uf_dir""",
     "/quiet"
 )
 
@@ -24,10 +26,24 @@ else {
 }
 $argument_list = $argument_list -replace "PATH_TO_MSI", "$SPLUNK_MSI"
 
-write-host "Installing Universal Forwarder, please wait.." -ForegroundColor Yellow
+## Unable to stop service
+# $SplunkForwarder = Get-Service -Name "SplunkForwarder" -ErrorAction SilentlyContinue
+# if ($null -ne $SplunkForwarder -and $SplunkForwarder.status -eq "Running") {
+#     write-host "Stopping Universal Forwarder, please wait.." -ForegroundColor Yellow
+#     stop-service -Name "SplunkForwarder"
+# }
+
+# Remove configs in C:\Splunk & $SPLUNK_HOME\etc\system\local
+# that may conflict with $SPLUNK_HOME\etc\apps\
+write-host "Uninstalling Universal Forwarder, please wait..." -ForegroundColor Yellow
+start-process msiexec.exe -argumentlist @("/x", "$SPLUNK_MSI", "/quiet") -wait -nonewwindow
+Rmf "$uf_dir" "C:\Splunk"
+
+write-host "Installing Universal Forwarder, please wait..." -ForegroundColor Yellow
 start-process msiexec.exe -argumentlist $argument_list -wait -nonewwindow
 
-# DEPLOYMENT_SERVER flag may be lost if UF crashes on first run
+# config file is more reliable,
+# DEPLOYMENT_SERVER msi flag may be lost if UF crashes during the initial start
 $config_file_content = '[deployment-client]
 disabled = 0
 sslVerifyServerCert = 1
@@ -37,27 +53,19 @@ sslCommonNameToCheck = splunkhostname.domain.example
 sslAltNameToCheck = splunkhostname
 
 [target-broker:deploymentServer]
-targetUri = https://splunkhostname:8089
+targetUri = https://splunkhostname.domain.example:8089
 '
 
-$uf_dirs = @("$env:programfiles\SplunkUniversalForwarder", "C:\Splunk")
-foreach ($uf_dir in $uf_dirs) {
-    if (test-path -path "$uf_dir\bin\splunk.exe") {
-        $config_file_directory = "$uf_dir\etc\apps\1-deploymentserver\local"
-        # after successful sync, this config will be replaced by
-        # deployment server's, which has the same content
-        $config_file = "$config_file_directory\deploymentclient.conf"
+if (test-path -path "$uf_dir\bin\splunk.exe") {
+    $config_file_directory = "$uf_dir\etc\apps\1-deploymentserver\local"
+    # after successful sync, this config will be replaced by
+    # deployment server's, which has the same content
+    $config_file = "$config_file_directory\deploymentclient.conf"
 
-        new-item -ItemType Directory $config_file_directory -Force | out-null
-        set-content $config_file $config_file_content
-        copy-item -path "ca-certificates.crt" -Destination "$config_file_directory\ca-certificates.crt"
+    new-item -ItemType Directory $config_file_directory -Force | out-null
+    set-content $config_file $config_file_content
+    copy-item -path "ca-certificates.crt" -Destination "$config_file_directory\ca-certificates.crt"
 
-        $uf_system_config = "$uf_dir\etc\system\local"
-
-        # Undo mistake
-        Rmf "$uf_system_config\deploymentclient.conf" "$uf_system_config\outputs.conf" "$uf_system_config\server.conf"
-
-        write-host "Restarting Splunk service to apply changes, please wait" -ForegroundColor Yellow
-        start-process "$uf_dir\bin\splunk.exe" -ArgumentList restart -NoNewWindow
-    }
+    write-host "Restarting Splunk service to apply changes, please wait..." -ForegroundColor Yellow
+    restart-service -name "SplunkForwarder"
 }

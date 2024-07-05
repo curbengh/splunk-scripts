00b987d8a67900737dac31911d068a1d61dc48aa
Ming Di Leom <2809763-curben@users.noreply.gitlab.com>
2024-07-05 11:18:40 +0000

    fix(setup): append to profile
    not replace

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,6 +2,18 @@
 
 # Run this in the splunk host as a normal user
 
-cp "profile" "$HOME/.profile"
+PROFILE="$HOME/.profile"
+if [ -f "$HOME/.bash_profile" ]; then
+  PROFILE="$HOME/.bash_profile"
+elif [ -f "$HOME/.zprofile" ]; then
+  PROFILE="$HOME/.zprofile"
+fi
+
+HAS_SPLUNK=$(grep -i "splunk" "$PROFILE" || [ $? = 1 ])
+
+if [ -z "$HAS_SPLUNK" ]; then
+  cat "profile" >> "$PROFILE"
+fi
+
 # load updated profile
-. "$HOME/.profile"
+. "$PROFILE"
